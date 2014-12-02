7bc580bee1c5b34b46c1229bd803638e0d926572
liwenjingbac@gmail,com <liwenjingbac@gmail,com>
2014-12-02 15:57:32 +0800

    d

--- test_script/26/26_control.rb
+++ test_script/26/26_control.rb
@@ -1,8 +1,8 @@
 begin 
-load './test_script/26/26_636572.rb'
-testResult = TestResult.new(:test_script_id=>6572,:test_plan_id=>26,:test_case_id=>63,:test_result_flag => true) 
+load './test_script/26/26_636577.rb'
+testResult = TestResult.new(:test_script_id=>6577,:test_plan_id=>26,:test_case_id=>63,:test_result_flag => true) 
 testResult.save 
 rescue Exception => e 
-testResult = TestResult.new(:test_script_id=>6572,:test_plan_id=>26,:test_case_id=>63,:test_result_content=>e.message,:test_result_flag => false) 
+testResult = TestResult.new(:test_script_id=>6577,:test_plan_id=>26,:test_case_id=>63,:test_result_content=>e.message,:test_result_flag => false) 
 testResult.save 
 end 
